2c06a89cd9b471fbafcfb4409a95ff269a58ed94
Kara Jelley <karajelley@gmail.com>
2024-12-03 07:03:54 -0500

    page and component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+// External Libraries 
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
+// Internal Libraries / Components
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import SignupPage from './pages/authentication/SignupPage';
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+
+// Styles / Assets
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <HashRouter>
-        <Routes>
-          <Route/>
-        </Routes>
-       </HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/landingpage" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<DashboardPage />} /> 
+        <Route path="*" element={<NotFoundPage />} /> 
+      </Routes>
+      <Footer />
     </>
   )
 }
 
-export default App
+export default App;
